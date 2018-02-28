d452d28292e8e7fee6375db5c230bbd2200108a5
Arslanov <maxim_arslanov@mail.ru>
2018-02-28 14:00:12 +0300

    Start
    add half database entities

---
+++ router.js
@@ -0,0 +1,35 @@
+var forDb = require('./forDb');
+
+function PathPage(path) {
+    switch (path) {
+        case '/':
+        case '/index':
+            return 'index.html';
+        case '/castle':
+            return 'index.html';
+        default:
+            return 'error.html'
+    }
+}
+
+
+function PathDb(parsedurl, res) {
+    switch (parsedurl.path) {
+        case '/caslte':
+            return forDb.findAl(forDb.Fraction, res);
+        case '/hero':
+            return forDb.findAl(forDb.Hero, res);
+        case '/magic':
+            return forDb.findAl(forDb.Magic, res);
+        case '/army':
+            return forDb.findAl(forDb.Army, res);
+        case '/caslte':
+            return forDb.findAl(forDb.Castle, res);
+        default:
+            res.end();
+    }
+}
+
+
+exports.PathPage = PathPage;
+exports.PathDb = PathDb;

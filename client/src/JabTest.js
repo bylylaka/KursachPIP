e61db8197613d3d084b2dd9c188ba537d92d286c
Arslanov <maxim_arslanov@mail.ru>
2018-04-23 23:36:51 +0300

    fix jabber bug

--- client/src/JabTest.js
+++ client/src/JabTest.js
@@ -1,30 +1,45 @@
 import socketIOClient  from 'socket.io-client';
+import axios from "axios/index";
 var React = require('react');
 
 export default class JabTe extends React.Component {
 
-    constructor() {
-        super()
+    constructor(props) {
+        super(props);
         this.state = {
-            endpoint: "http://localhost:8888" // this is where we are connecting to with sockets
+            endpoint: "http://localhost:8888", // this is where we are connecting to with sockets
+            isAuthenticated: false
+            // sockett: socketIOClient("http://localhost:8888")
         }
-    }
 
-    send = () => {
-        const socket = socketIOClient(this.state.endpoint)
-        socket.emit('change color', 'red')
-    }
-
-    render() {
-        const socket = socketIOClient(this.state.endpoint)
+        const socket = new socketIOClient("http://localhost:8888");
         socket.on('change color', (color) => {
-            console.log('Полученное сообщение: ' + color)
+            console.log('Полученное сообщение: ' + color);
             alert('Полученное сообщение: ' + color);
         })
+    }
+
+
+    componentDidMount() {
+        axios
+            .get(`/sessionUser`)
+            .then(session_user => {
+                this.setState({isAuthenticated : session_user.data });
+            })
+            .catch(error => console.log(error));
+    }
+
+
+    render() {
+        // if (this.state.isAuthenticated != false) {
+        //     this.state.sockett.on('change color', (color) => {
+        //         console.log('Полученное сообщение: ' + color);
+        //         alert('Полученное сообщение: ' + color);
+        //     })
+        // }
 
         return (
             <div style={{ textAlign: "center" }}>
-                <h1>БДСМ СТРАНИЦА</h1>
                 {/*<button onClick={() => this.send()}>Change Color</button>*/}
             </div>
         )

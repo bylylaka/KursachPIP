362d51d2ae172587ceb03e35f96e81692b4c2d03
Maxim Kuznetsov <maximus1998g@mail.ru>
2018-04-28 15:50:43 +0300

    q

--- client/src/menu.js
+++ client/src/menu.js
@@ -8,7 +8,7 @@ export default class Menu extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isAuthenticated: false
+            isAuthenticated: false,
         };
     }
 
